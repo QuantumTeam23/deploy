d75c5cac6195a36b7c90abcdec0ba16f97bfa14c
ciscoquirino <quirino1988@gmail.com>
2023-09-11 14:05:09 -0300

    add: login com autenticação por senha encriptada

--- ProjetoAPI/server/src/index.ts
+++ ProjetoAPI/server/src/index.ts
@@ -3,6 +3,7 @@ const bp = require('body-parser')
 const cors = require('cors');
 
 const app = express()
+const bcrypt = require("bcrypt")
 const port = 5001
 
 app.use(bp.json())
@@ -29,6 +30,15 @@ app.put('/editParceiro/:cnpj', editarParceiro);
 app.delete('/deletParceiro/:cnpj', deletarParceiro);
 app.get('/listParceiro', listAllParceiro);
 
+//ADMINISTRADOR
+app.post('/addAdministrador', cadastrarAdministrador);
+app.put('/editAdministrador/:email', editarAdministrador);
+app.delete('/deleteAdministrador/:email', deletarAdministrador);
+app.get('/listAdministrador', listAllAdministrador);
+
+//LOGIN
+app.post('/login', login);
+
 //CONEXÃO BANCO
 const DB = new Pool({
     connectionString: "postgres://ajisntze:gNdLfQIQWZ2gcQjKM9HZYV4MhGQU_bya@silly.db.elephantsql.com/ajisntze"
@@ -47,6 +57,58 @@ DB.connect().then(conn => {
         console.log('Servidor inicializado');
     });
 });
+
+//método para login 
+async function login(req, res) {
+    const {email, senha} = req.body;
+    const SQL1 = await connectionDB.query(`
+    SELECT * FROM Administradores 
+    WHERE administrador_email = '${email}'
+    `);
+    const SQL2 = await connectionDB.query(`
+    SELECT * FROM Estabelecimentos 
+    WHERE estabelecimento_email = '${email}'
+    `);
+    const SQL3 = await connectionDB.query(`
+    SELECT * FROM Parceiros 
+    WHERE parceiro_email = '${email}'
+    `);
+
+    if (SQL1.rowCount == 0 && SQL2.rowCount == 0 && SQL3.rowCount == 0){
+        return res.status(400).send('Usuário não encontrado, verifique o e-mail fornecido')
+    }
+    if (SQL1.rowCount == 1){
+        try {
+            if(await bcrypt.compare(senha, SQL1.rows[0].administrador_senha)) {
+                res.send("Sucesso")
+            }else{
+                res.send("Acesso não autorizado")
+            }
+        } catch {
+            res.status(500).send()
+        }
+    }else if(SQL2.rowCount == 1){
+        try {
+            if(await bcrypt.compare(senha, SQL2.rows[0].estabelecimento_senha)) {
+                res.send("Sucesso")
+            }else{
+                res.send("Acesso não autorizado")
+            }
+        } catch {
+            res.status(500).send()
+        }
+    }else if(SQL3.rowCount == 1){
+        try {
+            if(await bcrypt.compare(senha, SQL3.rows[0].parceiro_senha)) {
+                res.send("Sucesso")
+            }else{
+                res.send("Acesso não autorizado")
+            }
+        } catch {
+            res.status(500).send()
+        }
+    }
+}
 
 //CRUD ESTABELECIMENTO
 //Função para verificar se já tem cadastrado o cnpj
@@ -79,10 +141,11 @@ async function cadastrarEstabelecimento(req, res) {
         res.status(409).send({ msg: "Já existe um estabelecimento com esse CNPJ/CPF." });
     } else {
         try {
+            const hashSenha = await bcrypt.hash(senha, 10) 
             const SQL = `
                 INSERT INTO
                     Estabelecimentos("estabelecimento_razao_social","estabelecimento_nome_fantasia","estabelecimento_cnpj_cpf","estabelecimento_logradouro", "estabelecimento_logradouro_numero","estabelecimento_bairro","estabelecimento_cidade","estabelecimento_estado","estabelecimento_cep", "estabelecimento_regiao","estabelecimento_telefone","estabelecimento_saldo","estabelecimento_volume_comercializado_mes","estabelecimento_email", "estabelecimento_tipo", "estabelecimento_senha")
-                VALUES ('${nome}','${nomefantasia}','${cnpj}','${logradouro}', '${logradouroNumero}','${bairro}','${cidade}','${estado}','${cep}','${regiao}','${telefone}','${saldo}','${volume}','${email}','${tipo}','${senha}')
+                VALUES ('${nome}','${nomefantasia}','${cnpj}','${logradouro}', '${logradouroNumero}','${bairro}','${cidade}','${estado}','${cep}','${regiao}','${telefone}','${saldo}','${volume}','${email}','${tipo}','${hashSenha}')
             `
             const resultado = await connectionDB.query(SQL);
             console.log("Estabelecimento cadastrado com sucesso!");
@@ -208,10 +271,11 @@ async function cadastrarParceiro(req, res) {
         res.status(409).send({ msg: "Já existe um parceiro com esse CNPJ/CPF." });
     } else {
         try {
+            const hashSenha = await bcrypt.hash(senha, 10) 
             const SQL = `
                 INSERT INTO
                     Parceiros("parceiro_razao_social","parceiro_nome_fantasia","parceiro_cnpj_cpf","parceiro_logradouro", "parceiro_logradouro_numero","parceiro_bairro","parceiro_cidade","parceiro_estado","parceiro_cep", "parceiro_regiao","parceiro_telefone","parceiro_saldo","parceiro_cidades_atende","parceiro_data_inicio_operacao","parceiro_volume_coleta_mes","parceiro_email", "parceiro_tipo", "parceiro_senha")
-                VALUES ('${nome}','${nomefantasia}','${cnpj}','${logradouro}', '${logradouroNumero}','${bairro}','${cidade}','${estado}','${cep}','${regiao}','${telefone}','${saldo}', '${cidadeatendida}','${dataoperacao}','${volume}','${email}','${tipo}','${senha}')
+                VALUES ('${nome}','${nomefantasia}','${cnpj}','${logradouro}', '${logradouroNumero}','${bairro}','${cidade}','${estado}','${cep}','${regiao}','${telefone}','${saldo}', '${cidadeatendida}','${dataoperacao}','${volume}','${email}','${tipo}','${hashSenha}')
             `
             const resultado = await connectionDB.query(SQL);
             console.log("Parceiro cadastrado com sucesso!");
@@ -310,4 +374,121 @@ async function listAllParceiro(req, res) {
 
 //CRUD PARCEIROS
 
+//CRUD ADMINISTRADOR
+//Função para verificar se já tem cadastrado o mesmo email
+async function existeAdministrador(email) {
+    const res = await connectionDB.query(`
+        SELECT
+            *
+        FROM
+            Administradores
+        WHERE
+        administrador_email  = '${email}'
+    `);
 
+    var response = false
+    res.rows.forEach(administrador => {
+        if (administrador.administrador_email === email) {
+            console.log("Já existe um usuário cadastrado com esse e-mail!")
+            response = true
+        }
+    });
+    return response
+}
+
+async function cadastrarAdministrador(req, res) {
+    console.log("Requisição de cadastro de administrador recebida.");
+    const { nome, email, senha } = req.body;
+    //console.log(req.body)
+    const existeEmail = await existeAdministrador(email);
+    if (existeEmail) {
+        res.status(409).send({ msg: "Já existe um usuário cadastrado com esse e-mail" });
+    } else {
+        try {
+            //encriptar senha
+            const hashSenha = await bcrypt.hash(senha, 10) 
+
+            //salvar no bd com a senha encriptada
+            const SQL = `
+                INSERT INTO
+                    Administradores("administrador_nome","administrador_email","administrador_senha")
+                VALUES ('${nome}','${email}','${hashSenha}')
+            `
+            const resultado = await connectionDB.query(SQL);
+            console.log("Administrador cadastrado com sucesso!");
+            res.send({ msg: "Administrador cadastrado com sucesso!" });
+        } catch (error) {
+            console.error("Erro ao cadastrar Administrador:", error);
+            res.status(500).send({ msg: "Erro ao cadastrar administrador." });
+        }
+    }
+}
+
+
+async function editarAdministrador(req, res) {
+    console.log("Requisição de edição de administrador recebida.");
+    const email = req.params.email;
+    const { nome, senha } = req.body;
+    console.log(req.body);
+    try {
+        const SQL = `
+            UPDATE 
+                Administradores
+            SET
+                "administrador_nome" = '${nome}',
+                "administrador_senha" = '${senha}'
+            WHERE
+                administrador_email = '${email}'
+        `
+        const resultado = await connectionDB.query(SQL);
+        console.log("Administrador atualizado com sucesso!");
+        res.send({ msg: "Administrador atualizado com sucesso!" });
+    } catch (error) {
+        console.error("Erro ao editar administrador:", error);
+        res.status(500).send({ msg: "Erro ao editar administrador." });
+    }
+}
+
+
+async function deletarAdministrador(req, res) {
+    console.log("Requisição de exclusão de administrador recebida.");
+    const email = req.params.email;
+    try {
+
+        const SQL1 = `
+            DELETE FROM
+                Administradores
+            WHERE
+                administrador_email = '${email}'
+        `
+        await connectionDB.query(SQL1);
+        console.log("Cadastro excluído com sucesso!");
+        res.send({ msg: "Cadastro excluído com sucesso!" });
+    } catch (error) {
+        console.error("Erro ao excluir administrador:", error);
+        res.status(500).send({ msg: "Erro ao excluir administrador." });
+    }
+}
+
+
+async function listAllAdministrador(req, res) {
+    console.log("Requisição de listagem de administradores recebida.");
+    try {
+
+        const SQL = `
+    SELECT 
+        *
+    FROM 
+        Administradores
+    ORDER BY
+        administrador_id 
+    `
+        const resultado = await connectionDB.query(SQL);
+        console.log("Administradores listados com sucesso!");
+        res.send(resultado.rows);
+    } catch (error) {
+        console.error("Erro ao listar administradores:", error);
+        res.status(500).send({ msg: "Erro ao listar administradores." });
+    }
+}
+//CRUD ADMINISTRADORES
