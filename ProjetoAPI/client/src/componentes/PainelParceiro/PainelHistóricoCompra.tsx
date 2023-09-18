1ef05fb1286a119c793660e65576a0c2afd34a50
Nicholas Guilherme <102360510+NicholasGui29@users.noreply.github.com>
2023-09-18 15:01:17 -0300

    Movido a função de Editar usuário para as Navbars

--- ProjetoAPI/client/src/componentes/PainelParceiro/PainelHistóricoCompra.tsx
+++ ProjetoAPI/client/src/componentes/PainelParceiro/PainelHistóricoCompra.tsx
@@ -8,27 +8,6 @@ import { useNavigate } from 'react-router-dom';
 export default function PainelHistoricoCompra() {
   const navigate = useNavigate()
 
-  const handleClick = () => {
-    const id = localStorage.getItem('idParceiro'); // Substitua pelo ID correto do parceiro
-    fetch(`http://localhost:3001/read-by-id-to-edit/${id}`)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`Erro na solicitação: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(data => {
-        setTimeout(() => {
-          navigate('/editar-usuario');
-      }, 1100);
-        localStorage.setItem('parceiroData', JSON.stringify(data));
-
-      })
-      .catch(error => {
-        console.error('Erro ao buscar dados do parceiro:', error);
-      });
-  };
-
   return (
     <>
     <NavbarParceiro />
@@ -39,7 +18,6 @@ export default function PainelHistoricoCompra() {
         </div>
       </div>
       <h2>Histórico de Compras</h2>
-      <a href='#' onClick={handleClick}>editar</a>
       <TabelaHistoricoCompra />
     </div>
     <Footer />
