cf15bab0045d41ec92c94b2935ee7e1dc75288ab
Thalles Torres <57546200+ThallesTorres@users.noreply.github.com>
2023-11-05 14:56:52 -0300

    Add funcao sair na navbar de compraCredito

--- ProjetoAPI/client/src/componentes/Navbars/NavbarCompraCredito.tsx
+++ ProjetoAPI/client/src/componentes/Navbars/NavbarCompraCredito.tsx
@@ -5,32 +5,55 @@ import { useNavigate } from "react-router-dom";
 
 
 function NavbarCompraCredito() {
-  
+
+    const navigate = useNavigate();
+
+    const handleSair = () => {
+        Swal.fire({
+            title: 'Você tem certeza que deseja sair?',
+            text: 'Isso irá desconectar você da sua conta.',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#FFD700',
+            cancelButtonColor: 'red',
+            confirmButtonText: '<span style="color: white;">Sair</span>', // Estilizando o texto do botão Sair
+            cancelButtonText: '<span style="color: white;">Cancelar</span>', // Estilizando o texto do botão Cancelar
+            customClass: {
+                confirmButton: 'custom-confirm-button',
+                cancelButton: 'custom-cancel-button',
+            },
+        }).then((result) => {
+            if (result.isConfirmed) {
+                localStorage.clear()
+                navigate('/login')
+            }
+        });
+    }
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: '#136935' }}>
-            <div className="container" style={{ maxWidth: '90vw', maxHeight: '60px'}}>
+            <div className="container" style={{ maxWidth: '90vw', maxHeight: '60px' }}>
                 <a href="#" className="navbar-brand">
-                    <img src={require('../Imagens/logoEmpresa.png')} alt="Imagem" className="img-fluid"  style={{ maxWidth: '58px', maxHeight: '58px' }}  /> 
+                    <img src={require('../Imagens/logoEmpresa.png')} alt="Imagem" className="img-fluid" style={{ maxWidth: '58px', maxHeight: '58px' }} />
                 </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span style={{ display: 'flex', alignItems: 'center' }}><MenuLateralTransacao/></span>
+                    <span style={{ display: 'flex', alignItems: 'center' }}><MenuLateralTransacao /></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className='navbar-nav mt-auto'>
                         <li className="nav-item">
-                            <a className="nav-link text-white" href="#" style={{fontSize: '28px'}}>Transação</a>
+                            <a className="nav-link text-white" href="#" style={{ fontSize: '28px' }}>Transação</a>
                         </li>
                     </ul>
                     <ul className="navbar-nav ms-auto">
 
                         <li className="nav-item" style={{ marginRight: '1vw' }}>
-                            <img src={require('../Imagens/user.png')} alt="Imagem" className="img-fluid"  style={{ maxWidth: '36px', maxHeight: '36px' }}  /> 
+                            <img src={require('../Imagens/user.png')} alt="Imagem" className="img-fluid" style={{ maxWidth: '36px', maxHeight: '36px' }} />
                         </li>
-                 
+
                         <li className="nav-item">
                             <a href="#">
-                                <img src={require('../Imagens/icone-sair.png')} alt="Imagem" className="img-fluid" style={{ maxWidth: '36px', maxHeight: '36px' }} />
+                                <img src={require('../Imagens/icone-sair.png')} alt="Imagem" className="img-fluid" style={{ maxWidth: '36px', maxHeight: '36px' }} onClick={handleSair} />
                             </a>
                         </li>
                     </ul>
