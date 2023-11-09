cd7ea6601085eed79f937b38c0fa8a95448c16d8
Nicholas Guilherme <nicholas.teodoro@fatec.sp.gov.br>
2023-11-09 12:18:04 -0300

    Removação do componente isolado não utilizado
    Componente Barra de Pesquisada Isolada não utilizada foi removida, devido a reestruturação para cada página, para funcionar a função.

--- ProjetoAPI/client/src/componentes/PainelAdmin/PainelPrecoRegiao.tsx
+++ ProjetoAPI/client/src/componentes/PainelAdmin/PainelPrecoRegiao.tsx
@@ -1,8 +1,6 @@
 // HistoricoTransacoes.tsx
 import React, { useState } from 'react';
 import styles from '../styles/PainelLayoutGeral.module.css';
-import BarraPesquisa from './BarraPesquisa';
-import Tabelas2 from './TabelasHistTransacoes';
 import NavbarAdministrador from '../Navbars/NavbarAdministrador';
 import Footer from '../Footer/Footer';
 import TabelaPrecoRegiao from './TabelaPrecoRegiao';
